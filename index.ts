178f0f46b9b8c198e832afc9acb7bb5ee1f6a3af
Tomasz <tlesniakiewicz@gmail.com>
2025-01-07 18:49:31 +0100

    add new tools

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { runAgent } from './src/agent';
-import { z } from 'zod';
+import { tools } from './src/tools';
 
 const userMessage = process.argv[2];
 
@@ -9,17 +9,9 @@ if (!userMessage) {
   process.exit(1);
 }
 
-const weatherTool = {
-  name: 'get_weather',
-  description: 'use this to get weather',
-  parameters: z.object({
-    reasoning: z.string().describe('why did you pick up that tool?'), // add to every tool call! Makes sure that ai will thinkg about reason
-  }),
-};
-
-const response = await runAgent({
+await runAgent({
   userMessage,
-  tools: [weatherTool],
+  tools,
 });
 
 // console.log(JSON.stringify(response));
--- src/toolRunner.ts
+++ src/toolRunner.ts
@@ -1,6 +1,10 @@
 import type OpenAI from 'openai';
-
-const getWeather = () => 'very cold. 17deg.';
+import {
+  generateImage,
+  generateImageToolDefinition,
+} from './tools/generateImage';
+import { reddit, redditToolDefinition } from './tools/reddit';
+import { dadJoke, dadJokeToolDefinition } from './tools/dadJoke';
 
 export const runTool = async (
   toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall,
@@ -11,8 +15,15 @@ export const runTool = async (
     toolArgs: JSON.parse(toolCall.function.arguments),
   };
   switch (toolCall.function.name) {
-    case 'get_weather':
-      return getWeather();
+    case generateImageToolDefinition.name:
+      const image = await generateImage(input);
+      return image;
+
+    case dadJokeToolDefinition.name:
+      return dadJoke(input);
+
+    case redditToolDefinition.name:
+      return reddit(input);
 
     default:
       throw new Error(`Unknown tool: ${toolCall.function.name}`);
